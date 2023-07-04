09e373326630a0699f193fb9d07e030e7279f138
ZahariBakov <zaharibakov@gmail.com>
2023-07-04 10:23:31 +0300

    Add exercises "06_8_queens_puzzle" and "07_recursive_fibonacci". Update exercise "02_recursive_factorial".

--- Recursion/02_recursive_factorial.py
+++ Recursion/02_recursive_factorial.py
@@ -5,6 +5,17 @@ def calc_fact(n):
     return n * calc_fact(n - 1)
 
 
+def iterative_fact(n):
+    result = 1
+
+    for num in range(1, n + 1):
+        result *= num
+
+    return result
+
+
 num = int(input())
 
 print(calc_fact(num))
+print(iterative_fact(num))
+
---
+++ Recursion/07_recursive_fibonacci.py
@@ -0,0 +1,27 @@
+def calc_fib(number):
+    if number <= 1:
+        return 1
+
+    return calc_fib(number - 1) + calc_fib(number - 2)
+
+
+def iterative_fib(number):
+    fib0 = 1
+    fib1 = 1
+    result = 0
+
+    for _ in range(number - 1):
+        result = fib0 + fib1
+        fib0, fib1 = fib1, result
+
+    return result
+
+
+# print(calc_fib(10))
+# # print(calc_fib(50)) # This will hang!
+# print(iterative_fib(10))
+# print(iterative_fib(50))
+
+num = int(input())
+
+print(iterative_fib(num))
